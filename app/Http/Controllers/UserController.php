f0474f5c24dd961edc4447556d0b8b1816249763
66160372 <66160372@go.buu.ac.th>
2025-03-13 14:50:23 +0700

    feat(editController): add controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function create(Request $req){
+        $muser = new User();
+        $muser->fname = $req->input('fname');
+        $muser->lname = $req->input('lname');
+        $muser->role = $req->role;
+        $muser->email = $req->email;
+        $muser->save();
+        return redirect('/users');
+    }
+
+
+    function edit($id)
+    {
+        $user = User::find($id);
+        $data['user'] = $user;
+        return view('/user.edit', $data);
+    }
+    function edit_action(Request $req)
+    {
+        $muser = User::find($req->id);
+        $muser->fname = $req->fname;
+        $muser->lname = $req->lname;
+        $muser->role = $req->role;
+        $muser->saleSupervisor = $req->esaleSupervisor;
+        $muser->email = $req->email;
+        $muser->save();
+        return redirect('/users');
+    }
 }
