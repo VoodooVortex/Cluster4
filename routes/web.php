8a5f1e548119444cf902b19e664ff20bc42995cc
Pakkapon <Pakkapon2547@gmail.com>
2025-04-06 10:27:13 +0700

    fix(login): delete middleware

--- routes/web.php
+++ routes/web.php
@@ -30,45 +30,40 @@ Route::get('auth/google', [GoogleLoginController::class, 'redirectToGoogle'])->n
 
 Route::get('auth/google/callback', [GoogleLoginController::class, 'googleCallback'])->name('callback.google');
 
-Route::middleware([CheckGoogleLogin::class])->group(
-    function () {
+
+Route::get('/', [UserController::class, 'index']);
+
+//kuy mork
+Route::get('/cluster4/dashboard', [DashboardController::class, 'branchGrowthRate'])->name('dashboard.branch.growth');
+
+Route::get('/cluster4/home', [HomeController::class, 'index'])->name('home');
 
 
-        Route::get('/', [UserController::class, 'index']);
-
-        //kuy mork
-        Route::get('/cluster4/dashboard', [DashboardController::class, 'branchGrowthRate'])->name('dashboard.branch.growth');
-
-        Route::get('/cluster4/home', [HomeController::class, 'index'])->name('home');
+Route::get('/cluster4/manage-user', [UserController::class, 'index'])->name('manage.user');
 
 
-        Route::get('/cluster4/manage-user', [UserController::class, 'index'])->name('manage.user');
+Route::get('/cluster4/add-user', [UserController::class, 'add_user'])->name('add.user');
+Route::post('/cluster4/add-user', [UserController::class, 'create'])->name('create.user');
 
 
-        Route::get('/cluster4/add-user', [UserController::class, 'add_user'])->name('add.user');
-        Route::post('/cluster4/add-user', [UserController::class, 'create'])->name('create.user');
+Route::delete('/cluster4/delete-user', [UserController::class, 'delete_user'])->name('delete.user');
+
+Route::get('/cluster4/edit-user/{id}', [UserController::class, 'edit_user']);
+
+Route::put('/cluster4/edit-user', [UserController::class, 'edit_action'])->name('edit.user');
+
+Route::get('/cluster4/branchMyMap', [branchController::class, 'index'])->name('branchMyMap');
+
+Route::get('/cluster4/map', MapLocation::class)->name('map');
+
+// Aninthita 66160381
+Route::get('/cluster4/order-detail/{br_id}', [OrderController::class, 'order_detail']);
 
 
-        Route::delete('/cluster4/delete-user', [UserController::class, 'delete_user'])->name('delete.user');
+Route::get('/cluster4/order', [OrderController::class, 'index'])->name('order');
 
-        Route::get('/cluster4/edit-user/{id}', [UserController::class, 'edit_user']);
+Route::get('/cluster4/add-order', [OrderController::class, 'add_order']);
 
-        Route::put('/cluster4/edit-user', [UserController::class, 'edit_action'])->name('edit.user');
+Route::get('/cluster4/order-status', [OrderController::class, 'status'])->name('order.status');
 
-        Route::get('/cluster4/branchMyMap', [branchController::class, 'index'])->name('branchMyMap');
-
-        Route::get('/cluster4/map', MapLocation::class)->name('map');
-
-        // Aninthita 66160381
-        Route::get('/cluster4/order-detail/{br_id}', [OrderController::class, 'order_detail']);
-
-
-        Route::get('/cluster4/order', [OrderController::class, 'index'])->name('order');
-
-        Route::get('/cluster4/add-order', [OrderController::class, 'add_order']);
-
-        Route::get('/cluster4/order-status', [OrderController::class, 'status'])->name('order.status');
-
-        Route::get('/cluster4/employee', [UserController::class, 'Emp_GrowRate']);
-    }
-);
+Route::get('/cluster4/employee', [UserController::class, 'Emp_GrowRate']);
