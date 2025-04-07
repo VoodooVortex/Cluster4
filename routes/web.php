7d67dbac4379bb1f60ea0d6f98fed20edf2e814a
Pakkapon <Pakkapon2547@gmail.com>
2025-04-07 13:27:30 +0700

    delete /cluster4 in web.php

--- routes/web.php
+++ routes/web.php
@@ -20,62 +20,62 @@ use PHPUnit\Runner\HookMethod;
 
 // @author : Pakkapon Chomchoey 66160080
 
-Route::get('/cluster4/login', function () {
+Route::get('/login', function () {
     return view('login');
 })->name('login');
 
-Route::get('/cluster4/logout', function () {
+Route::get('/logout', function () {
     Session::forget('google_user');
     Session::flush();
     Auth::logout();
     return Redirect()->route('login');
 })->name('logout');
 
-Route::get('/cluster4/auth/google', [GoogleLoginController::class, 'redirectToGoogle'])->name('redirect.google');
+Route::get('/auth/google', [GoogleLoginController::class, 'redirectToGoogle'])->name('redirect.google');
 
-Route::get('/cluster4/auth/google/callback', [GoogleLoginController::class, 'googleCallback'])->name('callback.google');
+Route::get('/auth/google/callback', [GoogleLoginController::class, 'googleCallback'])->name('callback.google');
 
 Route::middleware([CheckGoogleLogin::class])->group(
     function () {
         Route::get('/', [HomeController::class, 'index'])->name('home');
-        Route::get('/cluster4/home', [HomeController::class, 'index'])->name('home');
+        Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-        Route::get('/cluster4/report/sales-team', [SalesTeamController::class, 'index'])->name('team');
+        Route::get('/report/sales-team', [SalesTeamController::class, 'index'])->name('team');
 
-        Route::get('/cluster4/report/sales-team{id}', [SalesTeamController::class, 'detail'])->name('team.detail');
-
-
-        Route::get('/cluster4/manage-user', [UserController::class, 'index'])->name('manage.user');
+        Route::get('/report/sales-team{id}', [SalesTeamController::class, 'detail'])->name('team.detail');
 
 
-        Route::get('/cluster4/add-user', [UserController::class, 'add_user'])->name('add.user');
-        Route::post('/cluster4/add-user', [UserController::class, 'create'])->name('create.user');
+        Route::get('/manage-user', [UserController::class, 'index'])->name('manage.user');
 
 
-        Route::delete('/cluster4/delete-user', [UserController::class, 'delete_user'])->name('delete.user');
+        Route::get('/add-user', [UserController::class, 'add_user'])->name('add.user');
+        Route::post('/add-user', [UserController::class, 'create'])->name('create.user');
 
 
-        Route::get('/cluster4/edit-user/{id}', [UserController::class, 'edit_user']);
+        Route::delete('/delete-user', [UserController::class, 'delete_user'])->name('delete.user');
 
-        Route::put('/cluster4/edit-user', [UserController::class, 'edit_action'])->name('edit.user');
 
-        Route::get('/cluster4/branchMyMap', [branchController::class, 'index'])->name('branchMyMap');
+        Route::get('/edit-user/{id}', [UserController::class, 'edit_user']);
 
-        Route::get('/cluster4/map', MapLocation::class)->name('map');
+        Route::put('/edit-user', [UserController::class, 'edit_action'])->name('edit.user');
+
+        Route::get('/branchMyMap', [branchController::class, 'index'])->name('branchMyMap');
+
+        Route::get('/map', MapLocation::class)->name('map');
 
         // Aninthita 66160381
-        Route::get('/cluster4/order-detail/{br_id}', [OrderController::class, 'order_detail']);
+        Route::get('/order-detail/{br_id}', [OrderController::class, 'order_detail']);
 
 
-        Route::get('/cluster4/order', [OrderController::class, 'index'])->name('order');
+        Route::get('/order', [OrderController::class, 'index'])->name('order');
 
-        Route::get('/cluster4/add-order', [OrderController::class, 'add_order']);
+        Route::get('/add-order', [OrderController::class, 'add_order']);
 
 
-        Route::get('/cluster4/order-status', [OrderController::class, 'status'])->name('order.status');
+        Route::get('/order-status', [OrderController::class, 'status'])->name('order.status');
 
-        Route::get('/cluster4/order-status', [OrderController::class, 'status'])->name('order.status');
+        Route::get('/order-status', [OrderController::class, 'status'])->name('order.status');
 
-        Route::get('/cluster4/report/team/{id}', [SalesTeamController::class, 'detail']);
+        Route::get('/report/team/{id}', [SalesTeamController::class, 'detail']);
     }
 );
